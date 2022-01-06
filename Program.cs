b58f5fe729754921974bf8c7fe765b09346ff0d5
Omega579 <88238063+Omega579@users.noreply.github.com>
2022-01-07 00:07:34 +0100

    made C# changes

--- Program.cs
+++ Program.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace csharp_console_app
 {
+    //here you can edit(in the namespace)
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			
+            string hiworld;
+            hiworld = "Hello World!";
+			System.Console.WriteLine(hiworld);
 		}
 	}
 }
